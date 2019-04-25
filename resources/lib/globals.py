649502760ab5a2c1912441aadb6baa3d0cae24ca
Ryan Bernard <ryanmbernard@gmail.com>
2019-04-25 18:34:54 -0400

    Revert "version bump"
    This reverts commit 1b7c08a1b8706368607c4601ffd861ff24a2e9ec.

--- resources/lib/globals.py
+++ resources/lib/globals.py
@@ -30,8 +30,6 @@ try:
     ENABLE_POOL = True
 except: ENABLE_POOL = False
 
-ENABLE_POOL = False # just for testing!!!
-
 # Plugin Info
 ADDON_ID      = 'plugin.video.slingtv'
 REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
